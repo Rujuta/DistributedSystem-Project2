2ffa183ea5802e8e9b719dc3ac6edb0ae3363c6d
Rujuta Deshpande <rdeshpa3@ugrad5.cs.jhu.edu>
2014-10-10 15:19:21 -0400

    Done with Start_mcast, skeleton of mcast

--- mcast.c
+++ mcast.c
@@ -2,91 +2,109 @@
 
 int main()
 {
-    struct sockaddr_in name;
-    struct sockaddr_in send_addr;
+	struct sockaddr_in name;
+	struct sockaddr_in send_addr;
 
-    int                mcast_addr;
+	int                mcast_addr;
 
-    struct ip_mreq     mreq;
-    unsigned char      ttl_val;
+	struct ip_mreq     mreq;
+	unsigned char      ttl_val;
 
-    int                ss,sr;
-    fd_set             mask;
-    fd_set             dummy_mask,temp_mask;
-    int                bytes;
-    int                num;
-    char               mess_buf[MAX_MESS_LEN];
-    char               input_buf[80];
+	int                ss,sr;
+	fd_set             mask;
+	fd_set             dummy_mask,temp_mask;
+	int                bytes;
+	int                num;
+	char               mess_buf[MAX_MESS_LEN];
+	char               input_buf[80];
 
-    mcast_addr = MCAST_ADDRESS /* (225.0.1.1) */;
+	mcast_addr = MCAST_ADDRESS /* (225.1.2.115*/;
 
-    sr = socket(AF_INET, SOCK_DGRAM, 0); /* socket for receiving */
-    if (sr<0) {
-        perror("Mcast: socket");
-        exit(1);
-    }
+	sr = socket(AF_INET, SOCK_DGRAM, 0); /* socket for receiving */
+	if (sr<0) {
+		perror("Mcast: socket");
+		exit(1);
+	}
 
-    name.sin_family = AF_INET;
-    name.sin_addr.s_addr = INADDR_ANY;
-    name.sin_port = htons(PORT);
+	name.sin_family = AF_INET;
+	name.sin_addr.s_addr = INADDR_ANY;
+	name.sin_port = htons(PORT);
 
-    if ( bind( sr, (struct sockaddr *)&name, sizeof(name) ) < 0 ) {
-        perror("Mcast: bind");
-        exit(1);
-    }
+	if ( bind( sr, (struct sockaddr *)&name, sizeof(name) ) < 0 ) {
+		perror("Mcast: bind");
+		exit(1);
+	}
 
-    mreq.imr_multiaddr.s_addr = htonl( mcast_addr );
+	mreq.imr_multiaddr.s_addr = htonl( mcast_addr );
 
-    /* the interface could be changed to a specific interface if needed */
-    mreq.imr_interface.s_addr = htonl( INADDR_ANY );
+	/* the interface could be changed to a specific interface if needed */
+	mreq.imr_interface.s_addr = htonl( INADDR_ANY );
 
-    if (setsockopt(sr, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void *)&mreq, 
-        sizeof(mreq)) < 0) 
-    {
-        perror("Mcast: problem in setsockopt to join multicast address" );
-    }
+	if (setsockopt(sr, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void *)&mreq, 
+				sizeof(mreq)) < 0) 
+	{
+		perror("Mcast: problem in setsockopt to join multicast address" );
+	}
 
-    ss = socket(AF_INET, SOCK_DGRAM, 0); /* Socket for sending */
-    if (ss<0) {
-        perror("Mcast: socket");
-        exit(1);
-    }
+	ss = socket(AF_INET, SOCK_DGRAM, 0); /* Socket for sending */
+	if (ss<0) {
+		perror("Mcast: socket");
+		exit(1);
+	}
 
-    ttl_val = 1;
-    if (setsockopt(ss, IPPROTO_IP, IP_MULTICAST_TTL, (void *)&ttl_val, 
-        sizeof(ttl_val)) < 0) 
-    {
-        printf("Mcast: problem in setsockopt of multicast ttl %d - ignore in WinNT or Win95\n", ttl_val );
-    }
+	ttl_val = 1;
+	if (setsockopt(ss, IPPROTO_IP, IP_MULTICAST_TTL, (void *)&ttl_val, 
+				sizeof(ttl_val)) < 0) 
+	{
+		printf("Mcast: problem in setsockopt of multicast ttl %d - ignore in WinNT or Win95\n", ttl_val );
+	}
 
-    send_addr.sin_family = AF_INET;
-    send_addr.sin_addr.s_addr = htonl(mcast_addr);  /* mcast address */
-    send_addr.sin_port = htons(PORT);
+	send_addr.sin_family = AF_INET;
+	send_addr.sin_addr.s_addr = htonl(mcast_addr);  /* mcast address */
+	send_addr.sin_port = htons(PORT);
 
-    FD_ZERO( &mask );
-    FD_ZERO( &dummy_mask );
-    FD_SET( sr, &mask );
-    FD_SET( (long)0, &mask );    /* stdin */
-    for(;;)
-    {
-        temp_mask = mask;
-        num = select( FD_SETSIZE, &temp_mask, &dummy_mask, &dummy_mask, NULL);
-        if (num > 0) {
-            if ( FD_ISSET( sr, &temp_mask) ) {
-                bytes = recv( sr, mess_buf, sizeof(mess_buf), 0 );
-                mess_buf[bytes] = 0;
-                printf( "received : %s\n", mess_buf );
-            }else if( FD_ISSET(0, &temp_mask) ) {
-                bytes = read( 0, input_buf, sizeof(input_buf) );
-                input_buf[bytes] = 0;
-                printf( "there is an input: %s\n", input_buf );
-                sendto( ss, input_buf, strlen(input_buf), 0, 
-                    (struct sockaddr *)&send_addr, sizeof(send_addr) );
-            }
-        }
-    }
+	FD_ZERO( &mask );
+	FD_ZERO( &dummy_mask );
+	FD_SET( sr, &mask );
+	FD_SET( (long)0, &mask );    /* stdin */
+	for(;;){
+	temp_mask=mask;
+	num = select( FD_SETSIZE, &temp_mask, &dummy_mask, &dummy_mask, NULL);
+	if(num>0){
+		if ( FD_ISSET( sr, &temp_mask) ) {
+			bytes = recv( sr, mess_buf, sizeof(mess_buf), 0 );
+			mess_buf[bytes] = 0;
+		//	printf( "received : %s\n", mess_buf );
+		//	printf("%d",strcmp(mess_buf,"start"));
+		//	printf("Done");
+			if(strcmp(mess_buf,"start")==0){
+				printf("\n Received Start\n");
+				break;
+			}
+		}
+	}
+	}
 
-    return 0;
+	/*for(;;)
+	{
+		temp_mask = mask;
+		num = select( FD_SETSIZE, &temp_mask, &dummy_mask, &dummy_mask, NULL);
+		if (num > 0) {
+			if ( FD_ISSET( sr, &temp_mask) ) {
+				bytes = recv( sr, mess_buf, sizeof(mess_buf), 0 );
+				mess_buf[bytes] = 0;
+				printf( "received : %s\n", mess_buf );
+			}else if( FD_ISSET(0, &temp_mask) ) {
+				bytes = read( 0, input_buf, sizeof(input_buf) );
+				input_buf[bytes] = 0;
+				printf( "there is an input: %s\n", input_buf );
+				sendto( ss, input_buf, strlen(input_buf), 0, 
+						(struct sockaddr *)&send_addr, sizeof(send_addr) );
+			}
+		}
+	}*/
+
+	return 0;
 
 }
 
--- start_mcast.c
+++ start_mcast.c
@@ -1,7 +1,52 @@
 #include "net_include.h"
 
+int main()
+{
+	struct sockaddr_in name;
+	struct sockaddr_in send_addr;
 
-int main(){
+	int                mcast_addr;
 
-return 0;
+	struct ip_mreq     mreq;
+	unsigned char      ttl_val;
+
+	int                ss;
+	fd_set             mask;
+	fd_set             dummy_mask,temp_mask;
+	int                bytes;
+	int                num;
+	char               mess_buf[MAX_MESS_LEN];
+	char               input_buf[80];
+
+	mcast_addr = MCAST_ADDRESS /* (225.1.2.115*/;
+
+	mreq.imr_multiaddr.s_addr = htonl( mcast_addr );
+
+	/* the interface could be changed to a specific interface if needed */
+	mreq.imr_interface.s_addr = htonl( INADDR_ANY );
+
+	ss = socket(AF_INET, SOCK_DGRAM, 0); /* Socket for sending */
+	if (ss<0) {
+		perror("Mcast: socket");
+		exit(1);
+	}
+
+	ttl_val = 1;
+	if (setsockopt(ss, IPPROTO_IP, IP_MULTICAST_TTL, (void *)&ttl_val, 
+				sizeof(ttl_val)) < 0) 
+	{
+		printf("Mcast: problem in setsockopt of multicast ttl %d - ignore in WinNT or Win95\n", ttl_val );
+	}
+
+	send_addr.sin_family = AF_INET;
+	send_addr.sin_addr.s_addr = htonl(mcast_addr);  /* mcast address */
+	send_addr.sin_port = htons(PORT);
+	strcpy(input_buf,"start");
+	sendto( ss, input_buf, strlen(input_buf), 0, 
+			(struct sockaddr *)&send_addr, sizeof(send_addr) );
+
+
+	return 0;
+
 }
+
